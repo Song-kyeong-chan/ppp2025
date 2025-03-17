3d860392d607486b6256b66402d4f3f69e73ae26
Kyeongchan Song <103410615+Song-kyeong-chan@users.noreply.github.com>
2025-03-17 18:03:50 +0900

    Add files via upload

--- title1.py
+++ title1.py
@@ -1,8 +1,7 @@
-#섭씨를 화씨로 변환하는 프로그램
-temp_c = 30
-temp_f = temp_c * 9 / 5 + 32
-print("섭씨{}도는 화씨{}도 입니다.".format(temp_c,temp_f))
+def temp_change(temp_c):
+    temp_f = temp_c * 9 / 5 + 32
+    print("섭씨{}도는 화씨{}도 입니다.".format(temp_c,temp_f))
+    return 0
 
-temp_c = 0
-temp_f = temp_c * 9 / 5 + 32
-print("섭씨{}도는 화씨{}도 입니다.".format(temp_c,temp_f))
+temp_c = int(input("온도를 입력하시오. : "))
+temp_change(temp_c)
